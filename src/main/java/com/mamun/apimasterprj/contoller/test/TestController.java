a78309cd5720e96d6716d95c5968a9c3c508b7ca
Al-Mamun <mamun7025@gmail.com>
2021-07-23 22:27:31 +0600

    Test controller added

--- src/main/java/com/mamun/apimasterprj/contoller/test/TestController.java
+++ src/main/java/com/mamun/apimasterprj/contoller/test/TestController.java
@@ -1,6 +1,7 @@
 package com.mamun.apimasterprj.contoller.test;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+//@CrossOrigin(origins="*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class TestController {
@@ -17,7 +20,7 @@ public class TestController {
     public Map<String, Object> getJSON() {
 
         Map<String, Object> returnJSON = new HashMap<>();
-        returnJSON.put("result", "success");
+        returnJSON.put("result", "success: Welcome to Home Page");
         return returnJSON;
 
     }
